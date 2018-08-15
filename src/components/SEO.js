22084a13629ae876bea793cddea89eb20442c91e
Matthias Jordan <matthias.jordan@surfair.com>
2018-08-15 16:56:43 -0700

    Move Branch.io code

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -148,39 +148,6 @@ class SEO extends Component {
         <meta name="twitter:image" content={image} />
         <meta name="twitter:description" content={description} />
 
-        <script type="text/javascript">{`
-       {
-         (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode banner closeBanner creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode".split(" "), 0);
-
-         branch.init('${process.env.BRANCH}');
-             function sendSMS(form) {
-                 var phone = form.phone.value;
-                 var linkData = {
-                     tags: [],
-                     channel: 'Website',
-                     feature: 'TextMeTheApp',
-                     data: {
-                         'foo': 'bar',
-                         '$desktop_url': 'https://surfair.com',
-                         '$ios_url': 'https://itunes.apple.com/us/app/surf-air/id592008851',
-                     }
-                 };
-                 var options = {};
-                 var callback = function(err, result) {
-                     if (err) {
-                         alert("Sorry, something went wrong.");
-                     }
-                     else {
-                         alert("Thank you. You should recieve a text message with a link to download the app soon.");
-                     }
-                 };
-                 branch.sendSMS(phone, linkData, options, callback);
-                 form.phone.value = "";
-             }
-       }
-   `}</script>
-
-
       </Helmet>
     )
   }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -24,6 +24,38 @@ const Template = ({ children }) => {
         <link rel="stylesheet" type="text/css" href={config.fonts} />
         <link rel="apple-touch-icon" href={config.siteIcon} />
         <link rel="shortcut icon" href={config.siteIcon} type="image/x-icon"/>
+
+        <script type="text/javascript">{`
+       {
+         (function(b,r,a,n,c,h,_,s,d,k){if(!b[n]||!b[n]._q){for(;s<_.length;)c(h,_[s++]);d=r.createElement(a);d.async=1;d.src="https://cdn.branch.io/branch-latest.min.js";k=r.getElementsByTagName(a)[0];k.parentNode.insertBefore(d,k);b[n]=h}})(window,document,"script","branch",function(b,r){b[r]=function(){b._q.push([r,arguments])}},{_q:[],_v:1},"addListener applyCode banner closeBanner creditHistory credits data deepview deepviewCta first getCode init link logout redeem referrals removeListener sendSMS setBranchViewData setIdentity track validateCode".split(" "), 0);
+
+         branch.init('${process.env.BRANCH}');
+             function sendSMS(form) {
+                 var phone = form.phone.value;
+                 var linkData = {
+                     tags: [],
+                     channel: 'Website',
+                     feature: 'TextMeTheApp',
+                     data: {
+                         'foo': 'bar',
+                         '$desktop_url': 'https://surfair.com',
+                         '$ios_url': 'https://itunes.apple.com/us/app/surf-air/id592008851',
+                     }
+                 };
+                 var options = {};
+                 var callback = function(err, result) {
+                     if (err) {
+                         alert("Sorry, something went wrong.");
+                     }
+                     else {
+                         alert("Thank you. You should recieve a text message with a link to download the app soon.");
+                     }
+                 };
+                 branch.sendSMS(phone, linkData, options, callback);
+                 form.phone.value = "";
+             }
+       }
+   `}</script>
       </Helmet>
 
       <ThemeProvider theme={theme}>
